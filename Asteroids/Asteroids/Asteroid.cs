70deb7c67cd8bebfe2973d7f7b29b001505153af
e6iskop <zhiganovtaras@gmail.com>
2018-12-13 20:52:49 +0400

    Дополнен класс Asteroid

--- Asteroids/Asteroids/Asteroid.cs
+++ Asteroids/Asteroids/Asteroid.cs
@@ -11,12 +11,12 @@ namespace AsteroidsGame
     /// <summary>
     /// Наследованный от BaseObject класс
     /// </summary>
-    class Asteroid : BaseObject
+    class Asteroid : BaseObject, ICloneable, IComparable
     {
         /// <summary>
         /// Свойство по умолчанию
         /// </summary>
-        public int Power { get; set; }
+        public int Power { get; set; } = 3;
 
         /// <summary>
         /// Конструктор на основе конструктора базового класса
@@ -29,6 +29,12 @@ namespace AsteroidsGame
             Power = 1;
         }
 
+        public object Clone()
+        {
+            Asteroid asteroid = new Asteroid(new Point(Pos.X, Pos.Y), new Point(Pos.X, Pos.Y), new Size(Size.Width, Size.Height))
+            { Power=Power};
+            return asteroid;
+        }
         /// <summary>
         /// Свой для этого класса перегруженный метод обновления 
         /// </summary>
@@ -44,6 +50,20 @@ namespace AsteroidsGame
         public override void Draw()
         {
             Game.Buffer.Graphics.FillEllipse(Brushes.White, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+        int IComparable.CompareTo(object obj)
+        {
+            if (obj is Asteroid temp)
+            {
+                if (Power > temp.Power)
+                    return 1;
+                if (Power < temp.Power)
+                    return -1;
+                else
+                    return 0;
+
+            }
+            throw new ArgumentException("Parameter is not а Asteroid!");
         }
     }
 }
